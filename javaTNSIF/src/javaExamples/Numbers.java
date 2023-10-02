4feca7ac52c4f31bfb3f4627d7718a2d595089a0
siris <siris@Saradha>
2023-10-02 10:34:22 +0530

    update commit

---
+++ javaTNSIF/src/javaExamples/Numbers.java
@@ -0,0 +1,25 @@
+package javaExamples;
+
+public class Numbers {
+	
+		public static void main(String[] args) {
+		    	int n=10;
+		    	int m=30;
+		    	int k=3;
+		        for (n = 10; n <= m; n++) {
+		            int digitSum = get(n);
+		            if (digitSum == k) {
+		                System.out.print(n+" ");
+		            }
+		        }
+		    }
+		    public static int get(int number) {
+		        int sum = 0;
+		        while (number != 0) {
+		            sum += number % 10;
+		            number /= 10;
+		        }
+		        return sum;
+		    }
+		}
+

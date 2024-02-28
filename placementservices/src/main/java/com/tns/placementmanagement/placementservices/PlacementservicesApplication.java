ff82d4e3740baf832273642f9bf1b37c593cf7ce
siris <siris@Saradha>
2024-02-28 20:37:46 +0530

    update

---
+++ placementservices/src/main/java/com/tns/placementmanagement/placementservices/PlacementservicesApplication.java
@@ -0,0 +1,13 @@
+package com.tns.placementmanagement.placementservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementservicesApplication.class, args);
+	}
+
+}
---
+++ placementservices/src/test/java/com/tns/placementmanagement/placementservices/PlacementservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.placementmanagement.placementservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
